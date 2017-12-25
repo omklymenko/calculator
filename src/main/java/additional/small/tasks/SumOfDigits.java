2b460253bc12c254c644de70151c94eceff65d3f
Olena Klymenko <olena.mklymenko@gmail.com>
2017-12-25 22:40:42 +0200

    Added sumOfDigits task

---
+++ src/main/java/additional/small/tasks/SumOfDigits.java
@@ -0,0 +1,30 @@
+package additional.small.tasks;
+
+public class SumOfDigits {
+
+    private static int calculateSumOfDigits(Integer number){
+        int result = 0;
+        while(number > 0){
+            result += number%10;
+            number = number/10;
+        }
+        return result;
+    }
+
+    private static int calculateSumOfDigits(String number){
+        int result = 0;
+        for(char digit : number.toCharArray()){
+            if(Character.isDigit(digit)) {
+                result += Character.getNumericValue(digit);
+            } else throw new IllegalArgumentException("Not a digit");
+        }
+        return result;
+    }
+
+    public static void main(String[] arg){
+        System.out.println("1 + 1 + 5 = " + calculateSumOfDigits("115"));
+        System.out.println("1 + 2 + 3 = " + calculateSumOfDigits(123));
+        System.out.println("1 + 2 + 3 = " + calculateSumOfDigits("1ghj"));
+    }
+
+}
