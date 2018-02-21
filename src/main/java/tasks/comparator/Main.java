47f4dfc0c1d0956239d3624121b434ccb1fb844e
omklymenko <olena.mklymenko@gmail.com>
2018-02-21 11:28:48 +0200

    Comparator + Builder task

--- src/main/java/tasks/comparator/Main.java
+++ src/main/java/tasks/comparator/Main.java
@@ -1,10 +1,21 @@
 package tasks.comparator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        Person person1 = new Person.PersonBuilder().name("Tom").build();
-        Person person2 = new Person.PersonBuilder().name("John").build();
-        Person person3 = new Person.PersonBuilder().name("Donna").build();
+        Person person1 = new Person.PersonBuilder().name("Tom").surname("Tom").dateOfBirth("2/12/1999").build();
+        Person person2 = new Person.PersonBuilder().name("John").surname("John").dateOfBirth("13/09/1993").build();
+        Person person3 = new Person.PersonBuilder().name("Donna").surname("Donna").dateOfBirth("21/10/1997").build();
+
+        List<Person> personList = new ArrayList<>(Arrays.asList(person1, person2, person3));
+        Collections.sort(personList, new Person.NameComparator());
+        System.out.println(personList.get(0).getName());
+        System.out.println(personList.get(1).getName());
+        System.out.println(personList.get(2).getName());
     }
 }
--- src/main/java/tasks/comparator/Person.java
+++ src/main/java/tasks/comparator/Person.java
@@ -1,5 +1,9 @@
 package tasks.comparator;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
 import java.util.Date;
 
 public class Person {
@@ -48,34 +52,54 @@ public class Person {
 
         // Spring (имя не set) ???
         public PersonBuilder name(String name) {
-            this.nestedName = nestedName;
+            this.nestedName = name;
             return this;
         }
 
         public PersonBuilder surname(String surname) {
-            this.nestedSurname = nestedSurname;
+            this.nestedSurname = surname;
             return this;
         }
 
-        public PersonBuilder dateOfBirth(Date dateOfBirth) {
-            this.nestedDateOfBirth = nestedDateOfBirth;
+        public PersonBuilder dateOfBirth(String dateOfBirth) {
+            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                this.nestedDateOfBirth= formatter.parse(dateOfBirth);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
             return this;
         }
 
         public PersonBuilder residence(String residence) {
-            this.nestedResidence = nestedResidence;
+            this.nestedResidence = residence;
             return this;
         }
 
         public PersonBuilder favouriteNumber(int favouriteNumber) {
-            this.nestedFavouriteNumber = nestedFavouriteNumber;
+            if(favouriteNumber >= -100 && favouriteNumber <= 100) {
+                this.nestedFavouriteNumber = favouriteNumber;
+            } else {
+                throw new IllegalArgumentException("Only -100 <= favouriteNumber <= 100 are acceptable");
+            }
             return this;
         }
 
         public Person build(){
-            return new Person(nestedName, nestedSurname,nestedDateOfBirth, nestedResidence, nestedFavouriteNumber);
+            if(nestedName != null && nestedSurname != null && nestedDateOfBirth != null) {
+                return new Person(nestedName, nestedSurname, nestedDateOfBirth, nestedResidence, nestedFavouriteNumber);
+            } else {
+                throw new IllegalArgumentException("Name, Surname and Date of Birth are required");
+            }
         }
 
+    }
+
+    static class NameComparator implements Comparator<Person> {
+        public int compare(Person p1, Person p2)
+        {
+            return p1.getName().compareTo(p2.getName());
+        }
     }
 
 }
